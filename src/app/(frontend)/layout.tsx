6af78cc4da1aef38e9504644b4a93784cd96d3a5
Donovan Jabbar <djabbar51@gmail.com>
2025-04-21 15:53:27 -0600

    feat: add ripple click and touch animation

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,37 +1,36 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import UtilityBar from "@components/UtilityBar/UtilityBar";
-import "./globals.scss";
-import Header from "@components/Header/Header";
-import NavBar from "@components/Nav/NavBar/NavBar";
-import Ripple from "@components/Ripple/Ripple";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import UtilityBar from '@components/UtilityBar/UtilityBar'
+import './globals.scss'
+import Header from '@components/Header/Header'
+import NavBar from '@components/Nav/NavBar/NavBar'
+import Ripple from '@components/Ripple/Ripple'
 
 const interSans = Inter({
-  variable: "--font-inter-sans",
-  subsets: ["latin"],
-});
-
+  variable: '--font-inter-sans',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Ace of Braids by Kate",
-  description: "Ace of Braids | Your expert for anything braids!",
-};
+  title: 'Ace of Braids by Kate',
+  description: 'Ace of Braids | Your expert for anything braids!',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{ 
-  children: React.ReactNode;
+}: Readonly<{
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={`${interSans.variable}`}>
-      <Ripple />
+        <Ripple />
         <Header>
           <UtilityBar />
           <NavBar />
         </Header>
-        {children}
+        <main className='main'>{children}</main>
       </body>
     </html>
-  );
+  )
 }
