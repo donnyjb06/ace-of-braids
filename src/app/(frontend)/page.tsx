4353d018223e545a717a0e5479eeb4a262bad777
Donovan Jabbar <djabbar51@gmail.com>
2025-04-21 17:49:38 -0600

    feat: implement and style landing page hero section

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,18 +1,22 @@
-import { headers as getHeaders } from 'next/headers.js'
 import { getPayload } from 'payload'
-import React from 'react'
-import { fileURLToPath } from 'url'
-
 import config from '@/payload.config'
+import Hero from '@/blocks/Hero/Hero';
 
-export default async function HomePage() {
-  const headers = await getHeaders()
+export default async function LandingPage() {
   const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
+  const payload = await getPayload({ config: payloadConfig });
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
+  const blocks = await payload.findGlobal({ slug: "landing" });
+
+  
   return(
-    <h1>Home</h1>
+    blocks.layout.map((block, index) => {
+      switch (block.blockType) {
+        case 'hero-block':
+          return <Hero key={index} {...block} />;
+        default: 
+        return null
+      }
+    })
   )
 }
