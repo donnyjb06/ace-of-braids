74f2b07553777c24fb437b344b741bc31831b17b
Donovan Jabbar <djabbar51@gmail.com>
2025-04-21 17:49:11 -0600

    feat: add hover and active state animations

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,58 @@
-import React, { HtmlHTMLAttributes } from 'react';
-import styles from './Button.module.scss';
-import clsx from 'clsx';
-import Image from 'next/image';
+import React, { HtmlHTMLAttributes, useState } from 'react'
+import styles from './Button.module.scss'
+import clsx from 'clsx'
+import Image from 'next/image'
 
 interface ButtonProps extends HtmlHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-  type: 'cta' | 'standard';
+  children: React.ReactNode
+  type: 'cta' | 'standard'
 }
 
-const Button = ({ className, type, children, ...props }: ButtonProps) => {
+const Button = ({ 
+  className, 
+  type, 
+  children, 
+  onClick, 
+  ...props 
+}: ButtonProps) => {
+  // State to track icon animation
+  const [isIconClicked, setIsIconClicked] = useState(false);
+
+  // Click handler
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => { 
+    setIsIconClicked(true); // Trigger animation
+
+    // Reset state after animation duration (300ms)
+    setTimeout(() => {
+      setIsIconClicked(false);
+    }, 150);
+
+    // Call the onClick passed via props
+    if (onClick) {
+      onClick(event); 
+    }
+  };
+
   return (
-    <button {...props} className={clsx(styles.button, className)}>
+    <button
+      {...props} 
+      className={clsx(styles.button, className)}
+      onClick={handleClick} 
+    >
       {children}
       {type === 'cta' && (
-        <Image src='/arrow-icon.svg' alt='arrow icon' height={24} width={24} />
+        <Image
+          src="/arrow-icon.svg"
+          alt="arrow icon"
+          height={24}
+          width={24}
+          className={clsx(styles.button__icon, {
+            [styles.iconClicked]: isIconClicked,
+          })}
+        />
       )}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
